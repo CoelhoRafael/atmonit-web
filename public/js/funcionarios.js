c938fc5479f1b402c8c970839e1c90c2a46962d5
abnerlucss <abner.santos@bandtec.com.br>
2021-11-25 10:50:56 -0300

    Desmockando id do funcionário

--- public/js/funcionarios.js
+++ public/js/funcionarios.js
@@ -15,14 +15,14 @@ function getAllEmployees(idCompany) {
                     tableBody.innerHTML +=
                         `
                     <tr>
-                        <td>01</td>
-                            <td>${element.employee_name}</td>
-                            <td>${element.office}</td>
-                            <td>${element.login}</td>
-                            <td>
-                                <span class="action_btn">
-                                  <button class="table-btn edit" onclick="openModal()">Editar</button>
-                                  <button class="table-btn remove">Remover</button>
+                        <td>${element.id_employee}</td>
+                        <td>${element.employee_name}</td>
+                        <td>${element.office}</td>
+                        <td>${element.login}</td>
+                        <td>
+                            <span class="action_btn">
+                                <button class="table-btn edit" onclick="openModal()">Editar</button>
+                                <button class="table-btn remove">Remover</button>
                             </span>
                         </td>
                     </tr>
