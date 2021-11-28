a701ed01dbb529b9165d731068b78cee9638da5b
abnerlucss <abner.santos@bandtec.com.br>
2021-11-28 11:31:03 -0300

    Adicionando configurações python

--- src/routes/atms.js
+++ src/routes/atms.js
@@ -1,10 +1,16 @@
 var express = require("express");
 var router = express.Router();
 var atmController = require("../controllers/atmController");
+var popularTimes = require("../controllers/popularTimes")
 
 
 router.get("/findAllTerminals/:idCompany", function (req, res) {
     atmController.findAllTerminals(req, res);
+})
+
+
+router.get("/getPopularTimes/", function (req, res) {
+    popularTimes.rodarScriptPython(req, res)
 })
 
 module.exports = router;
