6d8030603074d17c5276c39488838c17d049a51f
abnerlucss <abner.santos@bandtec.com.br>
2021-11-29 13:38:30 -0300

    AAAAAAAAAAAAA KCT

--- script-azure.sh
+++ script-azure.sh
@@ -1,10 +1,10 @@
 apt update
 sleep 1
-apt upgrade
+apt upgrade -y
 sleep 1
-apt-get install python3
+apt-get install python3 -y
 sleep 1
-apt-get install python3-pip
+apt-get install python3-pip -y
 sleep 1
 cd ..
 sleep 1
@@ -13,5 +13,7 @@ sleep 1
 git clone https://github.com/m-wrzr/populartimes.git
 sleep 1
 cd populartimes
+sleep 1
+pip3 install --upgrade setuptools
 sleep 1
 pip3 install .
