916f67c970fe58f0827bb14830404f0d6e7c3016
abnerlucss <abner.santos@bandtec.com.br>
2021-11-29 13:03:58 -0300

    Adicionando o script

---
+++ script-azure.sh
@@ -0,0 +1,17 @@
+apt update
+sleep 1
+apt upgrade
+sleep 1
+apt-get install python3
+sleep 1
+apt-get install python3-pip
+sleep 1
+cd ..
+sleep 1
+cd ..
+sleep 1
+git clone https://github.com/m-wrzr/populartimes.git
+sleep 1
+cd populartimes
+sleep 1
+pip3 install .
