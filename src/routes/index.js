e8e038a404f5406bc075289ea8c4a0c76fce46a3
abnerlucss <abner.santos@bandtec.com.br>
2021-11-29 11:11:05 -0300

    Configurando path

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,7 @@ router.get("/", function (req, res) {
 router.get("/popularTimes/", (req, res, next) => {
 
     var options = {
-        scriptPath: "/home/site/repository",
+        scriptPath: "~/home/site/repository",
         args: ["ChIJ0RGdBvFZzpQRQeWcrwlhk8s"]
     }
 
