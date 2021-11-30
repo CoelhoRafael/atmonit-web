e2e729d6aefc2c02c95c862859a24d603b5bd7ff
abnerlucss <abner.santos@bandtec.com.br>
2021-11-30 20:46:28 -0300

    Adicionando loads

--- public/js/dashboard_fila.js
+++ public/js/dashboard_fila.js
@@ -3,7 +3,7 @@ var dados
 
 function plotarGrafico() {
     dados = {
-        labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
+        labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 00],
         datasets: [{
             yAxisID: 'y-fila',
             label: 'Uso',
